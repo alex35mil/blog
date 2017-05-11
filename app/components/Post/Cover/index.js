30b21cc06f6ffb1262a9b94c3d471330bad9efd2
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-05-11 19:22:26 +0800

    Add cover loader

--- app/components/Post/Cover/index.js
+++ app/components/Post/Cover/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import React from 'react';
+import cn from 'classnames';
 
 import { A } from 'app/components';
 
@@ -14,12 +15,49 @@ type $Props = {|
   |},
 |};
 
-export const Cover = ({ cover, credit }: $Props) => (
-  <div className={styles.cover} style={cover && { backgroundImage: `url(${cover})` }}>
-    {cover && <div className={styles.overlay} />}
-    {credit &&
-      <div className={styles.credit}>
-        Artwork: <A unstyled href={credit.url} targetBlank>{credit.author}</A>
-      </div>}
-  </div>
-);
+type $State = {| isLoaded: boolean |};
+
+export class Cover extends React.Component {
+  props: $Props;
+  state: $State = { isLoaded: false };
+
+  componentDidMount = () => {
+    this.preloadCover();
+  };
+
+  preloadCover = () => {
+    if (!this.props.cover) return;
+
+    let image = new Image();
+
+    image.onload = () => {
+      this.setState({ isLoaded: true });
+      if (image) image.src = '';
+      image = null;
+    };
+    image.src = this.props.cover;
+  };
+
+  render = () => {
+    const { cover, credit } = this.props;
+    const { isLoaded } = this.state;
+
+    if (!cover) return <div className={styles.background} />;
+
+    return (
+      <div className={styles.background}>
+        <div
+          className={cn(styles.cover, isLoaded ? styles.loaded : styles.loading)}
+          style={{ backgroundImage: `url(${cover})` }}
+        >
+          <div className={styles.overlay} />
+          {credit &&
+            <div className={styles.credit}>
+              Artwork:{' '}
+              <A unstyled href={credit.url} targetBlank>{credit.author}</A>
+            </div>}
+        </div>
+      </div>
+    );
+  };
+}
