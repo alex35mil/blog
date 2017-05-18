85ba096d6d9962d369d91e701dc54e0affc8912d
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-05-18 23:35:57 +0800

    Add caching

--- server.js
+++ server.js
@@ -4,15 +4,38 @@ import express from 'express';
 import favicon from 'serve-favicon';
 import path from 'path';
 
+import type { $Request, $Response, NextFunction as $NextFunction } from 'express';
+
 import { locations, WEB_PORT } from 'config';
 import App from 'app/shell/server/App';
 
 const server = express();
 
-server.use(express.static(locations.publicPath));
-server.use(favicon(path.join(locations.publicPath, 'favicon.ico')));
-server.use('/', App);
+// Settings
+server.set('etag', 'weak');
+server.set('x-powered-by', false); // for security reasons
 
+// Static assets
+server.use(
+  express.static(locations.publicPath, {
+    maxAge: '1y', // assets are fingerprinted, so caching all the way
+  }),
+);
+
+// Favicon
+server.use(favicon(path.join(locations.publicPath, 'favicon.ico')));
+
+// App
+server.use(
+  '/',
+  (req: $Request, res: $Response, next: $NextFunction) => {
+    res.header('Cache-Control', 'private, no-cache');
+    next();
+  },
+  App,
+);
+
+// Listen
 server.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
   console.log('=> 🚀  WEB server is running on port', WEB_PORT);
